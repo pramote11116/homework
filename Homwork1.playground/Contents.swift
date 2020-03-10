50469193449d05272edecab202cfe332014b4c0b
MTA-MAC <mta-mac@MTA-MACs-iMac-28.local>
2020-03-10 12:35:30 +0700

    ข้อ1

---
+++ Homwork1.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var number1:Double=5
+var number2:Double=4
+
+var summary:Double = number1 + number2	
+var different:Double = number1 - number2
+var multiply:Double = number1 * number2
+var divide:Double = number1 / number2
